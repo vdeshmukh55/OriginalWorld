be58fb87ed4c9ae4213430f2c3a527e4ad5d523e
vdeshmukh55 <123566370+vdeshmukh55@users.noreply.github.com>
2023-01-26 13:51:27 +0530

    Create de.java

---
+++ de.java
@@ -0,0 +1,9 @@
+public class De
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Create file on Remote Repository");
+    System.out.println("This File added by A developer");
+    System.out.println("Below Button Commit new file ... click on it");
+  }
+}
