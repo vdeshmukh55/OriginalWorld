5a04ba189ecbe969cef965460246c064f697a316
vdeshmukh55 <123566370+vdeshmukh55@users.noreply.github.com>
2023-01-26 16:34:21 +0530

    Create Abcde.java

---
+++ Abcde.java
@@ -0,0 +1,12 @@
+public class ABCDE
+{ 
+  public void test
+  {
+    System.out.println("This is Test Method");
+  }
+  public static void main(String[] args)
+  {
+    ABCDE  a = new ABCDE();
+    a.test();
+  }
+}
